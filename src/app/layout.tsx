fa700b6da562ab2c2696da6bbb01b2bf224268a7
Abdul Majeed <johnharris5934@gmail.com>
2024-10-19 22:49:50 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <meta name="google-site-verification" content="6wHwczpjeXS8BFTheq-HDoZBKJYwu4RrvNgkeWbHfXo" />
         <Header/>
         <br /><br /><br />
         {children}
